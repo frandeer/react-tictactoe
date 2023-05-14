dc97047ebaa926af0d9a50c7f297c7c0698e7399
ho <ho@wedb.co.kr>
2023-05-14 18:51:36 +0900

    initial commit

---
+++ src/components/TickWrap.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TickWrap = () => {
+    return (
+        <div>
+            <div className="tick-wrap"></div>
+        </div>
+    );
+}
+
+export default TickWrap;
